367787603985854ac28aef088e7e70223c6c301c
Diego <105529723+Yiufi@users.noreply.github.com>
2022-06-09 09:25:11 -0300

    Full application

---
+++ CapturarIP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CapturarIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
+            {
+                Console.WriteLine("No Network Available");
+            }
+
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+
+            var ippaddress = host
+                .AddressList
+                .FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+            Console.WriteLine(ippaddress);
+        }
+    }
+}
